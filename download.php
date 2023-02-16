f5db80f8947f51050a80abd7c732c77ac20bcac1
İlyas Özkurt <ilyasozkurt@6harf.com>
2023-02-16 22:29:24 +0300

    cover photo for non-tv set issue solved.

--- download.php
+++ download.php
@@ -26,11 +26,13 @@ $responseDom = str_get_html($responseBody);
 $initialData = preg_match('/ytInitialData = (.*);<\/script>/', $responseBody, $matches);
 $initialData = json_decode($matches[1], true);
 
-$channelCover = end($initialData['header']['c4TabbedHeaderRenderer']['tvBanner']['thumbnails'])['url'] ?? null;
+$channelCover = end($initialData['header']['c4TabbedHeaderRenderer']['banner']['thumbnails'])['url'] ?? null;
 
 $channelLogo = $responseDom->find('meta[property="og:image"]', 0)->getAttribute('content') ?? null;
 if ($downloadType == 'cover') {
-    $downloadURL = $channelCover;
+    preg_match('/(.*?)\=w/', $channelCover, $matches);
+    $fullCoverURL = $matches[1] . '=w4000';
+    $downloadURL = $fullCoverURL;
 } else {
     $downloadURL = $channelLogo;
 }
